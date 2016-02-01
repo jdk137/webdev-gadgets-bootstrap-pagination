85e26517a45be116570bb7f777740160d30c0554
fahui.jdk <fahui.jdk@taobao.com>
2016-02-01 14:40:19 +0800

    commit js and demos

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # webdev-gadgets-bootstrap-pagination
-pagination component based on bootstrap style
+pagination component based on bootstrap pagination
+
+
+simple usage:
+
+html, this component did nothing but insert html code into div.pagination
+``` html
+    <div class="text-center">
+      <div class="pagination"></div>
+    </div>
+```
+
+javascript
+``` js
+  $('.pagination1').bsPagination({
+    totalRecord: 200,
+    recordPerPage: 10,
+    recentPage: 1
+  });
+```
+
+More complex demos
---
+++ page.js
@@ -0,0 +1,60 @@
+$.fn.bsPagination = function (config) {
+  var $container = $(this);
+  // 2 required params
+  // all records number
+  var totalRecord = config.totalRecord;
+  // recent page
+  var recentPage = config.recentPage || 1;
+
+  // optional params
+  // setLinkAttr to add to every page link, it's userdefined.
+  var setLinkAttr = config.setLinkAttr || function (page) {
+    return 'data-page="' + page + '"';
+  };
+  // records number in one page, default is 10
+  var recordPerPage = config.recordPerPage || 10;
+  // pages number show in one batch, default is 5
+  // for example: << 6 7 8 9 10 >> means pagePerBatch is 5.
+  // << 4 5 6 >> means pagePerBatch is 3.
+  var pagePerBatch = config.pagePerBatch || 5;
+  var showFirstPage = config.showFirstPage || false; // boolean, show first page link or not
+  var showLastPage = config.showFirstPage || false; // boolean, show last page link or not
+  var firstPageWord = config.firstPageWord || 'First'; // first page word
+  var lastPageWord = config.lastPageWord || 'Last'; // last page word
+
+  // data process;
+  var totalPage = Math.ceil(totalRecord / recordPerPage); //total page number
+  var totalBatch = Math.ceil(totalPage / pagePerBatch); // total batch number
+  var recentBatch = Math.ceil(recentPage / pagePerBatch); // recent batch
+  recentPage = Math.min(recentPage, totalPage); // recentPage should not be larger than totalPage
+  var minPage = (recentBatch - 1) * pagePerBatch + 1; // min page of recent batch
+  var maxPage = Math.min(recentBatch * pagePerBatch, totalPage);  // max page of recent batch
+  var showLeft = recentBatch > 1; // boolean, show left-batch-link or not
+  var showRight = recentBatch < totalBatch; // boolean, show right-batch-link or not
+
+  // draw
+  var html = '';
+  var active = '';
+  var i;
+  if (totalPage <= 1) { //not show if totalPage <= 1
+    $container.html('');
+    return;
+  }
+  if (showFirstPage && showLeft) {
+    html += '<li class="first-page"><a ' + setLinkAttr(1) + '>' + firstPageWord + '</a></li>';
+  }
+  if (showLeft) {
+    html += '<li class="left-batch"><a ' + setLinkAttr(minPage - 1) + '>«</a></li>';
+  }
+  for (i = minPage; i <= maxPage; i++) {
+    active = (i === recentPage) ? 'class="active"' : '';
+    html += '<li ' + active + '><a ' + setLinkAttr(i) + '>' + i + '</a></li>';
+  }
+  if (showRight) {
+    html += '<li class="right-batch"><a ' + setLinkAttr(maxPage + 1) + '>»</a></li>';
+  }
+  if (showLastPage && showRight) {
+    html += '<li class="last-page"><a ' + setLinkAttr(totalPage) + '>' + lastPageWord + '</a></li>';
+  }
+  $container.html(html);
+};
