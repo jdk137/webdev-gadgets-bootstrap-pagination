f9b166d3272434f3873793a23333602608e1b2eb
fahui.jdk <fahui.jdk@taobao.com>
2016-02-02 10:35:42 +0800

    prevPageWord to prevBatchWord

--- page.js
+++ page.js
@@ -17,12 +17,12 @@ $.fn.bsPagination = function (config) {
   // for example: << 6 7 8 9 10 >> means pagePerBatch is 5.
   // << 4 5 6 >> means pagePerBatch is 3.
   var pagePerBatch = config.pagePerBatch || 5;
-  var prevPageWord = config.prevPageWord || '<'; // previous page word
-  var nextPageWord = config.nextPageWord || '>'; // next page word
+  var prevBatchWord = config.prevBatchWord || '<<'; // previous batch word
+  var nextBatchWord = config.nextBatchWord || '>>'; // next batch word
   var showFirstPage = config.showFirstPage || false; // boolean, show first page link or not
   var showLastPage = config.showFirstPage || false; // boolean, show last page link or not
-  var firstPageWord = config.firstPageWord || '<<'; // first page word
-  var lastPageWord = config.lastPageWord || '>>'; // last page word
+  var firstPageWord = config.firstPageWord || '<<<'; // first page word
+  var lastPageWord = config.lastPageWord || '>>>'; // last page word
 
   // data process;
   var totalPage = Math.ceil(totalRecord / recordPerPage); //total page number
@@ -46,14 +46,14 @@ $.fn.bsPagination = function (config) {
     html += '<li class="first-page"><a ' + setLinkAttr(1) + '>' + firstPageWord + '</a></li>';
   }
   if (showLeft) {
-    html += '<li class="left-batch"><a ' + setLinkAttr(minPage - 1) + '>' + prevPageWord + '</a></li>';
+    html += '<li class="left-batch"><a ' + setLinkAttr(minPage - 1) + '>' + prevBatchWord + '</a></li>';
   }
   for (i = minPage; i <= maxPage; i++) {
     active = (i === currentPage) ? 'class="active"' : '';
     html += '<li ' + active + '><a ' + setLinkAttr(i) + '>' + i + '</a></li>';
   }
   if (showRight) {
-    html += '<li class="right-batch"><a ' + setLinkAttr(maxPage + 1) + '>' + nextPageWord + '</a></li>';
+    html += '<li class="right-batch"><a ' + setLinkAttr(maxPage + 1) + '>' + nextBatchWord + '</a></li>';
   }
   if (showLastPage && showRight) {
     html += '<li class="last-page"><a ' + setLinkAttr(totalPage) + '>' + lastPageWord + '</a></li>';
